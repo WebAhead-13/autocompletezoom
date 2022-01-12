cf5d11a7258588acde6cca4dda5096ad24206fe3
doaa <doaaziadat@gmail.com>
2022-01-12 15:19:28 +0200

    search.js prints name started with the prefic in the search

--- public1/Search.js
+++ public1/Search.js
@@ -1,10 +1,34 @@
 
-console.log("hello from js ");
+var data ;
+input= document.querySelector("#myInput");
 
 fetch("/employees")
   .then(response => {
     if (!response.ok) throw new Error(response.status);
     return response.json()
   })
-  .then (data => console.log(data))
+  .then (json => {
+     data=json;
+     console.log(json)
+    })
   .catch(console.error);
+
+  input.addEventListener("input", function(e) {
+    console.log(e.target.value)
+    const text=e.target.value;
+    if(e.target.value) {
+    for(let i=0;i<data.Employees.length;i++)
+    {
+      let name=data.Employees[i].firstName;
+      if(name.startsWith(text))
+        console.log(name)
+
+    }
+  }
+
+
+  })
+
+
+
+
